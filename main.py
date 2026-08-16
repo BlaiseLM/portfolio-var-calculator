8220e7f0e9c6be527f1451a3fc6360694baf43bc
Blaise <blaise.mahamoodally@gmail.com>
2026-08-16 08:53:57 -0400

    Added CVaR calculation

--- main.py
+++ main.py
@@ -13,15 +13,15 @@ interval = os.getenv("INTERVAL")
 num_shares = pd.Series({
     "MSFT": 100, 
     "AAPL": 200, 
-    "GOOG": 300
-    })
+    "KO": 300, 
+})
 
 portfolio = yf.Tickers(num_shares.index.tolist())
 
 # DataFrame creation
 df = portfolio.download(period=period, interval=interval)
 
-# Portfolio daily log returns
+# Daily log returns
 df_closing_prices = df["Close"].dropna()
 prev_closing_prices = df_closing_prices.shift(1)
 
@@ -33,3 +33,13 @@ daily_weight = prev_values.div(prev_portfolio_value, axis=0)
 
 weighted_sum = (simple_daily_return * daily_weight).sum(axis=1)
 log_daily_portfolio_return = np.log(1 + weighted_sum)
+
+# Historical CVaR at 95% confidence
+return_at_5th_percentile = np.percentile(log_daily_portfolio_return, 5)
+mean_5_percentiles = np.mean(log_daily_portfolio_return[log_daily_portfolio_return <= return_at_5th_percentile])
+
+current_portfolio_value = df_closing_prices.iloc[-1] @ num_shares
+
+mean_5_percentiles_in_dollars = current_portfolio_value * (np.exp(mean_5_percentiles) - 1)
+
+print(mean_5_percentiles_in_dollars)
